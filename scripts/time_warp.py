242beb7b0ed8aa9263cd49fccd32960ca7e8a3d4
adamjrbaker <adam@adambakerart.com>
2023-05-26 16:38:50 -0400

    Basic timewarp setup to create and add to warp.

---
+++ scripts/time_warp.py
@@ -0,0 +1,112 @@
+""" Selected node time wrapper"""
+
+import re
+
+import maya.cmds
+
+
+def create_warp(warp_name=None, anti_warp=False):
+    """ Create warp nodes.
+
+    Args:
+        warp_name (str | None): Custom name of time warp curve.
+        anti_warp (bool | False): If warp node should be a anti warp.
+
+    Returns:
+        maya node name of warp time curve.
+    """
+
+    min_time = maya.cmds.playbackOptions(query=True, minTime=True)
+    max_time = maya.cmds.playbackOptions(query=True, maxTime=True)
+
+    curve_name = 'atk_WarpCurve'
+    connection_name = "atk_warpSettings"
+
+    if warp_name:
+        curve_name = warp_name
+        connection_name = '{}_warpSettings'.format(warp_name)
+
+    if anti_warp:
+        curve_name = '{}_AntiWarp'.format(curve_name)
+
+    warp_node = maya.cmds.createNode('animCurveTT', name=curve_name)
+    maya.cmds.setAttr(warp_node + ".preInfinity", 1)
+    maya.cmds.setAttr(warp_node + ".postInfinity", 1)
+
+    # Anti warp is created based on the scene time warp
+    if anti_warp:
+        for frame in range(min_time, max_time + 1):
+            time = maya.cmds.getAttr('time1.outTime', time=frame)
+            maya.cmds.setKeyframe(warp_node, time=time, value=frame)
+    else:
+        maya.cmds.setKeyframe(warp_node, time=min_time, value=min_time, inTangentType="spline", outTangentType="spline")
+        maya.cmds.setKeyframe(warp_node, time=max_time, value=max_time, inTangentType="spline", outTangentType="spline")
+
+    status_node = maya.cmds.createNode('WarpStatus', name=re.sub(curve_name, warp_node, connection_name))
+
+    # Set connections for toggle node
+    maya.cmds.connectAttr(warp_node + ".output", status_node + ".warpInput", force=True)
+    # Connect real time to the alternate condition
+    maya.cmds.connectAttr("time1.outTime", status_node + ".timeInput", force=True)
+
+    return warp_node
+
+
+def apply_warp(warp_node):
+    """ Apply Warp to selected nodes.
+
+    Args:
+        warp_node (str): Name of warp node to apply the on the selected objects
+
+    Returns:
+        Bool If applied
+    """
+
+    selection = maya.cmds.ls(selection=True, dag=True, long=True)
+
+    if not selection:
+        return False
+
+    input_nodes = []
+
+    for node in selection:
+        input_nodes = list(set(input_nodes + get_inputs(node)))
+
+    for connection in input_nodes:
+        node_type = maya.cmds.nodeType(connection)
+
+        if node_type in ["animCurveTU", "animCurveTA", "animCurveTL"]:
+            maya.cmds.connectAttr("{}.output".format(warp_node), '{}.input'.format(connection), force=True)
+
+    return True
+
+
+def get_inputs(node):
+    """ Get input nodes of selected node.
+
+    Args:
+        node (str): Name of maya node to check.
+
+    Returns:
+        List of connections of that node.
+    """
+
+    results = []
+
+    # Get geo nodes connections.
+    geo_inputs = maya.cmds.listConnections(node, source=True, destination=False,
+                                           skipConversionNodes=True, type="geometryFilter") or []
+    # Get connected animation curves.
+    anim_curves = maya.cmds.listConnections(node, source=True, destination=False,
+                                            skipConversionNodes=True, type="animCurve") or []
+    # Make sure we aren't having any duplicates.
+    inputs = list(set(geo_inputs + anim_curves))
+
+    if not inputs:
+        return results
+
+    # We can make sure we have no connections of connections.
+    for connection in inputs:
+        results += get_inputs(connection)
+
+    return list(set(inputs + results))
